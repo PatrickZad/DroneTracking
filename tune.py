9754e0da6766583aed6e66c3fad190fc63380829
Patrick <dawnerz@163.com>
2020-03-25 22:10:50 +0800

    on rocks

--- tune.py
+++ tune.py
@@ -44,8 +44,8 @@ def train(is_coarse_available=False):
                          freeze_body=2, weights_path=model_path)  # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=model_base)
-    checkpoint = ModelCheckpoint(os.path.join(model_base , 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
-                                 monitor='val_loss', save_weights_only=True, save_best_only=True, period=5)
+    checkpoint = ModelCheckpoint(os.path.join(model_base, 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'),
+                                 monitor='val_loss', save_weights_only=True, save_best_only=False, period=5)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
     num_train = len(os.listdir(train_img_dir))
